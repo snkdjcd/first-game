62a115f78a3d8b643708725d3c055ce214069415
snkdjcd <149696249+snkdjcd@users.noreply.github.com>
2023-11-16 18:33:05 +0200

    Create main_window.py

---
+++ main_window.py
@@ -0,0 +1,10 @@
+from PyQt5.QtCore import*
+from PyQt5.QtWidgets import*
+
+
+window_main = QWidget()
+window_main.resize(600, 500)
+window_main.setWindowTitle("Memory card")
+window_main.move(300, 300)
+
+btn_menu = QPushButton("Меню")
